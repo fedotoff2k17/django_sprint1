1925c31bde2a6e3d3679cfa6e072de013f80e773
Федотов Илья Викторович <44693038+fedotoff2k17@users.noreply.github.com>
2024-11-23 18:43:29 +0700

    update

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -7,8 +7,7 @@ posts = [
         'date': '30 сентября 1659 года',
         'category': 'travel',
         'text': '''Наш корабль, застигнутый в открытом море
-                  страшным штормом, потерпел крушение...
-                  ''',
+                  страшным штормом, потерпел крушение...''',
     },
     {
         'id': 1,
@@ -16,20 +15,26 @@ posts = [
         'date': '1 октября 1659 года',
         'category': 'not-my-day',
         'text': '''Проснувшись поутру, я увидел, что наш корабль сняло
-                  с мели приливом и пригнало гораздо ближе к берегу...
-                  ''',
+                  с мели приливом и пригнало гораздо ближе к берегу...''',
+    },
+    {
+        'id': 2,
+        'location': 'Остров отчаянья',
+        'date': '25 октября 1659 года',
+        'category': 'not-my-day',
+        'text': '''Всю ночь и весь день шёл дождь и дул сильный
+                  порывистый ветер...''',
     },
 ]
 
-
 def index(request):
-    return render(request, 'index.html', {'posts': posts})
+    return render(request, 'blog/index.html', {'posts': reversed(posts)})
 
 
 def post_detail(request, id):
-    post = next(post for post in posts if post['id'] == id)
-    return render(request, 'detail.html', {'post': post})
+    post = next((post for post in posts if post['id'] == id), None)
+    return render(request, 'blog/detail.html', {'post': post})
 
 
 def category_posts(request, category_slug):
-    return render(request, 'category.html', {'category_slug': category_slug})
+    return render(request, 'blog/category.html', {'category_slug': category_slug})
