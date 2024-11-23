443153e604bd56c03550aca2b931b30f0e35afc5
Федотов Илья Викторович <44693038+fedotoff2k17@users.noreply.github.com>
2024-11-23 18:09:18 +0700

    update

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -21,12 +21,15 @@ posts = [
     },
 ]
 
+
 def index(request):
     return render(request, 'index.html', {'posts': posts})
+
 
 def post_detail(request, id):
     post = next(post for post in posts if post['id'] == id)
     return render(request, 'detail.html', {'post': post})
+
 
 def category_posts(request, category_slug):
     return render(request, 'category.html', {'category_slug': category_slug})
